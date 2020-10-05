2937e8d9388578423601b736aae4481eb0ffcfa8
Chetna Malhotra <chetna.malhotra@wavelabs.ai>
2020-10-06 00:53:42 +0530

    Front end with Home and Product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import {BrowserRouter as Router,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
+    
+      <Header/>
+    <main className='py-3'>
+      <Container>
+        <Route exact path='/' component={HomeScreen}/>
+        <Route path='/product/:id' component={ProductScreen}/>
+      </Container>
+    </main>
      
-    </>
+     <Footer/>
+  
+    </Router>
   );
 }
 
